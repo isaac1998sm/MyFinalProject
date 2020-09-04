ef35a241739b3f9892ab0b596360f9d19986eb67
isaac1998sm <isaacsalmanpour@gmail.com>
2020-09-04 02:51:16 -0700

    finish templates
    finish templates

--- restaurant/src/products/models.py
+++ restaurant/src/products/models.py
@@ -2,9 +2,10 @@ import random
 import os
 from django.db import models
 from django.db.models.signals import pre_save, post_save
-
+from django.urls import reverse
 
 from .utils import unique_slug_generator
+
 
 def get_filename_ext(filepath):
     base_name = os.path.basename(filepath)
@@ -23,12 +24,14 @@ def upload_image_path(instance, filename):
             final_filename=final_filename
             )
 
+
 class ProductQuerySet(models.query.QuerySet):
     def active(self):
         return self.filter(active=True)
 
     def featured(self):
         return self.filter(featured=True, active=True)
+
 
 class ProductManager(models.Manager):
     def get_queryset(self):
@@ -47,6 +50,7 @@ class ProductManager(models.Manager):
         return None
 
 
+
 class Product(models.Model):
     title           = models.CharField(max_length=120)
     slug            = models.SlugField(blank=True, unique=True)
@@ -59,14 +63,19 @@ class Product(models.Model):
     objects = ProductManager()
 
     def get_absolute_url(self):
-        return "/products/{slug}/".format(slug=self.slug)
+        #return "/products/{slug}/".format(slug=self.slug)
+        return reverse("products:detail", kwargs={"slug": self.slug})
 
     def __str__(self):
+        return self.title
+
+    def __unicode__(self):
         return self.title
 
 
 def product_pre_save_receiver(sender, instance, *args, **kwargs):
     if not instance.slug:
         instance.slug = unique_slug_generator(instance)
+
 
 pre_save.connect(product_pre_save_receiver, sender=Product)
--- restaurant/src/restaurant/urls.py
+++ restaurant/src/restaurant/urls.py
@@ -14,15 +14,31 @@ from django.contrib import admin
 #         ProductFeaturedDetailView
 #         )
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.conf.urls import url, include
+from django.contrib import admin
+
+# from products.views import (
+#         ProductListView,
+#         product_list_view,
+#         ProductDetailView,
+#         ProductDetailSlugView,
+#         product_detail_view,
+#         ProductFeaturedListView,
+#         ProductFeaturedDetailView
+#         )
+
 from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
-    url(r'^$', home_page),
-    url(r'^about/$', about_page),
-    url(r'^contact/$', contact_page),
-    url(r'^login/$', login_page),
-    url(r'^register/$', register_page),
-    url(r'^products/', include("products.urls")),
+    url(r'^$', home_page, name='home'),
+    url(r'^about/$', about_page, name='about'),
+    url(r'^contact/$', contact_page, name='contact'),
+    url(r'^login/$', login_page, name='login'),
+    url(r'^register/$', register_page, name='register'),
+    url(r'^products/', include("products.urls", namespace='products')),
     # url(r'^featured/$', ProductFeaturedListView.as_view()),
     # url(r'^featured/(?P<pk>\d+)/$', ProductFeaturedDetailView.as_view()),
     # url(r'^products/$', ProductListView.as_view()),
